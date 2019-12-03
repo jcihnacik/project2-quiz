f4b99c2e5c807a44c71ed7dd73a71faf262a455e
jcihnacik <jcihnacik@gmail.com>
2019-12-02 20:34:28 -0500

    characters added and quiz app written

---
+++ app/data/characters.js
@@ -0,0 +1,48 @@
+var characters = [
+    {
+        name: "Rodrigo, the Would-Be Pirate King",
+        bio: "You're bold and confident, and have been roaming the Several Seas since you were a small child. You've got a great vision to be the biggest and baddest pirate leader there ever was-- but first, you have to assemble a crew and overthrow your rival.",
+        scores: [
+        "1",
+        "2",
+        "2",
+        "1",
+        "1"
+    ]
+    },
+
+    {
+        name: "Lurakka, the Magician of the Mount",
+        bio: "You have a mysterious dream that leads you to the top of the tallest mountain. There, it is promised, you will find your calling... what will you invoke with your newfound powers?",
+        scores: [
+            "2",
+            "3",
+            "3",
+            "4",
+            "4"
+        ]
+    },
+    {
+        name: "Gurghl, the Dungeon Dweller",
+        bio: "No one has seen your face in ten years, but plenty have your enchanting song and followed it down the dungeons, and were never to be seen again. How did you get there and what other legends and adventures can you inspire?",
+        scores: [
+            "2",
+            "4",
+            "1",
+            "2",
+            "2"
+        ]
+    },
+
+    {
+        name: "Cassaria, the Forest Witch",
+        bio: "You are talented witch living just outside the long arm of the king's law in the forest. You are a talented spell caster and the subject of many local legends... but no one knows where you came from. What is your origin story, and why does the king seem to have a personal problem with you?",
+        scores: [
+            "1",
+            "1",
+            "4",
+            "3",
+            "3"
+        ]
+    }
+]
